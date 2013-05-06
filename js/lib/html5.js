8c39cca61fa9ab0a351fedc4dcad20e80aa90ca7
Luiz Henrique Zanão Kota <luiz.zk@gmail.com>
2013-05-06 20:34:57 -0300

    Modular Boilerplate 1.0
    Modular boilerplate 1.0
    - Basic structure
    - Less
    - Lib

---
+++ js/lib/html5.js
@@ -0,0 +1,12 @@
+/*
+ * Title: HTML5 Tags
+ *
+ * Description: Create html5 tags for old browsers
+ * Adaptation: Kota, Luiz
+ * Version: 1.0
+ */
+
+/*
+ * HTML5
+ */
+(function(){if(!/*@cc_on!@*/0)return;var e = "abbr,article,aside,audio,canvas,datalist,details,eventsource,figure,footer,header,hgroup,mark,main,menu,meter,nav,output,progress,section,time,video".split(','),i=e.length;while(i--){document.createElement(e[i])}})()
